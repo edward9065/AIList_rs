edd90237748a8c26bb32171908ee7a3123838b01
Edward Chen <edward9065@gmail.com>
2023-09-11 18:57:45 -0400

    first version of AIList code complete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use std::fmt;
+
 struct Interval {
     pub start: usize,
     pub end: usize,
+}
+
+impl fmt::Display for Interval {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.start, self.end)
+    }
 }
 
 struct AIList {
@@ -41,9 +49,14 @@ impl AIList {
         while i > 0 {
             i-=1;
             if interval.start > self.ends[i] { //this means that there is no interssection
-
+                if interval.start > self.max_ends[i] { //there is no further intersection
+                    return results_list;
+                }
             } else {
-
+                results_list.push(Interval {
+                    start: self.starts[i],
+                    end: self.ends[i],
+                })
             }
         }
         return results_list;
@@ -53,10 +66,29 @@ impl AIList {
 }
 
 
-fn main(){
-    let v = [1,2,3,4,5];
-    let i = v.partition_point(|&x| x < 0);
-    println!("{i}");
+fn main() {
+    let intervals = vec![
+        Interval { start: 1, end: 2 },
+        Interval { start: 3, end: 8 },
+        Interval { start: 5, end: 7 },
+        Interval { start: 7, end: 20},
+        Interval { start: 9, end: 10},
+        Interval { start: 13, end: 15},
+        Interval { start: 15, end: 16},
+        Interval { start: 19, end: 30},
+        Interval { start: 22, end: 24},
+        Interval { start: 24, end: 25},
+        Interval { start: 26, end: 28},
+        Interval { start: 32, end: 38},
+        Interval { start: 34, end: 36},
+        Interval { start: 38, end: 40},
+    ];
+
+    let ai_list = AIList::new(intervals);
+    let hi = ai_list.query(&Interval {start: 18, end: 20});
+    for element in hi.iter() {
+        print!("{}, ", element);
+    }
 }
 
 
