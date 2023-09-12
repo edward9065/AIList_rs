789a3215e7405c4024482bceeb5157316a30612c
Edward Chen <edward9065@gmail.com>
2023-09-12 00:14:30 -0400

    first draft of decompose

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,44 @@ struct AIList {
     starts: Vec<usize>, 
     ends: Vec<usize>,
     max_ends: Vec<usize>,
+    header_list: Vec<usize>,
 }
 
 impl AIList {
-    fn new(mut intervals: Vec<Interval>) -> AIList {
+    fn new(mut intervals: Vec<Interval>, minimum_coverage_length: usize) -> AIList {
         // in the future, clone and sort...
         intervals.sort_by_key(|key| key.start);
        
         let mut starts: Vec<usize> = Vec::new();
         let mut ends: Vec<usize> = Vec::new();
         let mut max_ends: Vec<usize> = Vec::new();
+        let mut header_list: Vec<usize> = Vec::new();
+
+        loop {
+            
+            let interval_list = intervals;
+            let results = decompose(interval_list, minimum_coverage_length);
+            
+            starts.append(results[0]);
+            ends.append(results[1]);
+            max_ends.append(results[2]);
+
+            interval_list = results[3];
+
+            if(interval_list.len() == 0) {
+                break;
+            } else {
+                if let Some(last) = header_list.last() {
+                    header_list.push(results[0].len()+last);
+                }
+            }
+                
+        }
+
 
         let mut max: usize = 0; 
+
+
 
         for interval in intervals.iter() {
             max = if max > interval.end { max } else { interval.end };
@@ -38,8 +64,42 @@ impl AIList {
         AIList {
             starts, 
             ends,
-            max_ends
+            max_ends,
+            header_list
         }
+    }
+
+    fn decompose(intervals: Vec<Interval>, minimum_coverage_length: usize) -> (Vec<usize>, Vec<usize>, Vec<usize>, Vec<Interval>) {
+        // look at the next minL*2 intervals
+        let mut starts: Vec<usize> = Vec::new();
+        let mut ends: Vec<usize> = Vec::new();
+        let mut max_ends: Vec<usize> = Vec::new();
+        let mut l2: Vec<Interval> = Vec::new();
+        
+        for (index, interval) in intervals.enumerate() {
+            let mut count = 0;
+            for i in 1..(minimum_coverage_length*2) {
+                match intervals.get(index+i) {
+                    Some(interval2) => if interval.end > interval2.end { count += 1; },
+                    None => break,
+                }
+            }
+            if count >= minimum_coverage_length {
+                l2.push(interval);
+            } else {
+                starts.push(interval.start);
+                ends.push(interval.end)
+            }
+        }
+
+        let mut max: usize = 0; 
+
+        for end in ends.iter() {
+            max = if max > *end { max } else { *end };
+            max_ends.push(max); 
+        }
+
+        (starts, ends, max_ends, l2)
     }
 
     fn query(&self, interval: &Interval) -> Vec<Interval> {
